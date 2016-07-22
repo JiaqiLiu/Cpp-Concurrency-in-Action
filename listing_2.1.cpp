5012cc0ed5f82691a32e2ec0f6036037e5958051
jiaqi <liujiaqi09@gmail.com>
2016-07-22 11:31:40 +0800

    learn

--- listing_2.1.cpp
+++ listing_2.1.cpp
@@ -1,8 +1,13 @@
 #include <thread>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
 
 void do_something(int& i)
 {
     ++i;
+    cout << "i:  " << i << endl;
 }
 
 struct func
@@ -20,16 +25,19 @@ struct func
     }
 };
 
-
 void oops()
 {
     int some_local_state=0;
     func my_func(some_local_state);
     std::thread my_thread(my_func);
+    usleep(1e3);
     my_thread.detach();
 }
 
 int main()
 {
+    cout << "Start" << endl;
     oops();
+    cout << "Finished..." << endl;
+    usleep(1e3);
 }
